d52507f6158fff29066c4cc44b8cb57b15bf660e
alessandrasinigaglia <alessandra.sinigaglia06@gmail.com>
2024-12-10 17:22:41 +0100

    feat: add section and new const

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
 // Dom Elements
 const priceForKm = 0.21;
 const formname = document.getElementById('name');
-const formkm = Number(document.getElementById('km').value)
 const formage = document.getElementById('age');
 const generate = document.getElementById('generate');
 const cancel = document.getElementById('cancel');
+const passeggero = document.getElementById('passeggero');
+const offerta = document.getElementById('offerta');
+const carrozza = document.getElementById('carrozza');
+const codcp = document.getElementById('codcp');
+const ticketprice = document.getElementById('ticketprice');
+const ticket = document.getElementById('ticket');
+
 
 const name = formname.value
 
 generate.addEventListener("click", function (){
+    const formkm = Number(document.getElementById('km').value)
     let price = formkm * priceForKm;
     if (age <= 18) {
         price -= price * 0.20; 
