9284d7940e565e8f2235145edb377416dc45c4aa
alessandrasinigaglia <alessandra.sinigaglia06@gmail.com>
2024-12-10 16:39:39 +0100

    commit per jacopo

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Dom Elements
 const priceForKm = 0.21;
 const formname = document.getElementById('name');
-const formkm = document.getElementById('km');
+const formkm = Number(document.getElementById('km').value)
 const formage = document.getElementById('age');
 const generate = document.getElementById('generate');
 const cancel = document.getElementById('cancel');
@@ -9,7 +9,7 @@ const cancel = document.getElementById('cancel');
 const name = formname.value
 
 generate.addEventListener("click", function (){
-    let price = km * priceForKm;
+    let price = formkm * priceForKm;
     if (age <= 18) {
         price -= price * 0.20; 
     if (age >= 65) {
